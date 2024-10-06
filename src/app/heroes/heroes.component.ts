cb9a946efeb5619fdc81ab167107e9502238241b
Pedz <pedroenrico16@gmail.com>
2024-10-06 19:14:07 -0300

    Add Angular Material And hero-detail Component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine'
-  };
+export class HeroesComponent {
+  heroes = HEROES
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    
+    this.selectedHero = hero; 
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
-
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Pedro'},
+    { id: 2, name: 'Cacku'},
+    { id: 3, name: 'Jorge'},
+    { id: 4, name: 'Xka'},
+    { id: 5, name: 'Hik'},
+    { id: 6, name: 'Pedz'},
+    { id: 7, name: 'Monk'},
+    { id: 8, name: 'Mask'},
+    { id: 9, name: 'Mers'},
+    { id: 10, name: 'Souls'}
+];
