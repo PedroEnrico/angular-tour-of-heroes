66a2cfaacde374e302532bfcc064e8bfc3e61005
Pedz <pedroenrico16@gmail.com>
2024-10-17 00:14:54 -0300

    Modified the core system organization and implemented CoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,26 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './core/material/material.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
     DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    FlexLayoutModule,
     BrowserAnimationsModule,
+
     AppRoutingModule,
-    MaterialModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,38 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { MaterialModule } from './material/material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
+
+const COMPONENTS = [
+  MessagesComponent,
+  ToolbarComponent
+]
+
+const MODULES = [FlexLayoutModule, MaterialModule, RouterModule]
+
+@NgModule({
+  declarations: [COMPONENTS],
+  imports: [
+    MODULES,
+    CommonModule
+  ],
+  exports: [
+    COMPONENTS,
+    MODULES,
+    MaterialModule
+  ]
+})
+export class CoreModule {
+  //@Optional(): Essa anotação indica que o parâmetro parentModule pode ser opcional
+  // @SkipSelf(): Essa anotação indica que, se o módulo pai for encontrado no módulo atual, ele deve ser ignorado
+  //Verifica se o módulo já foi carregado anteriormente e, caso isso ocorra, lançar uma exceção.
+  constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule has already been loaded. Import this Module in the AppModule');
+    }
+  }
+}
