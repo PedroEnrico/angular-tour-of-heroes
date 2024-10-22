bbb21c5891402ceee4034d1e3776a027ef1f8eed
Pedz <pedroenrico16@gmail.com>
2024-10-22 00:48:02 -0300

    Created a new interceptor to get errors of http requests

---
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MessageService } from '../services/message.service';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+
+  constructor(private messageService: MessageService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        let errorMsg = '';
+
+        if (err.error instanceof ErrorEvent) { // instanceOf = Se for do tipo ErrorEvent
+          errorMsg = `Error: ${err.error.message}`;
+        } else {
+          errorMsg = `Error Code: ${err.status}, Message: ${err.message}`;
+        }
+
+        this.messageService.add(errorMsg);
+        return throwError(() => new Error(errorMsg));
+      })
+    );
+  }
+}
