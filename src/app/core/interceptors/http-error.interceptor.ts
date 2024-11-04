dd42361aa3fb0055e3d33c19ddc9fefaec306aa5
Pedz <pedroenrico16@gmail.com>
2024-11-04 00:26:06 -0300

    Created Token flow

--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -9,6 +9,7 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { MessageService } from '../services/message.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
@@ -18,10 +19,19 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse) => {
+
+        if (!environment.production) {
+          console.log(err);
+        }
+
         let errorMsg = '';
 
         if (err.error instanceof ErrorEvent) { // instanceOf = Se for do tipo ErrorEvent
           errorMsg = `Error: ${err.error.message}`;
+        } else if(Array.isArray(err.error) && err.error.length) {
+          errorMsg = `Error: ${err.error[0]}`
+        } else if (err.error.errors) {
+          errorMsg = `Error: ${err.error.errors}`
         } else {
           errorMsg = `Error Code: ${err.status}, Message: ${err.message}`;
         }
---
+++ src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    // localStorage -> área especifica do browser para salvar dados.
+    // Busco um token no localStorage
+    let token = localStorage.getItem('token'); // Sempre que usar o mesmo browser, apontado para mesma url irá apontar para o token criado.
+
+    // Valida se existe o token, se não tiver cria
+    if (!token) {
+      token = this.generateToken();
+      // Seta o token no localStorage
+      localStorage.setItem('token', token);
+    }
+
+    // clona a request e adiciona o Header Authorization como token
+    request = request.clone({
+      setHeaders: {
+        Authorization: token
+      }
+    })
+
+    return next.handle(request);
+  }
+
+  private generateToken(): string {
+    // Gerar um token
+    return Math.random().toString(36).substring(2,12);
+  }
+}
